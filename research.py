618152e0b3a9aac019401822de120b1069191236
Utsal Shrestha <utsal.shres@gmail.com>
2017-11-27 18:52:13 -0800

    weight and bias tensors added; shape not added yet

--- research.py
+++ research.py
@@ -12,7 +12,22 @@ X = tf.placeholder('float64', [None, n_input])
 Y = tf.placeholder('float32', [None])
 
 # Weights and bias
-
+weights = {
+    'h11': tf.Variable(tf.random_normal([])),
+    'h12': tf.Variable(tf.random_normal([])),
+    'h13': tf.Variable(tf.random_normal([])),
+    'h14': tf.Variable(tf.random_normal([])),
+    'h21': tf.Variable(tf.random_normal([])),
+    'h22': tf.Variable(tf.random_normal([]))    
+}
+biases = {
+    'b11': tf.Variable(tf.random_normal([])),
+    'b12': tf.Variable(tf.random_normal([])),
+    'b13': tf.Variable(tf.random_normal([])),
+    'b14': tf.Variable(tf.random_normal([])),
+    'b21': tf.Variable(tf.random_normal([])),
+    'b22': tf.Variable(tf.random_normal([]))
+}
 
 # Define loss
 loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = out_layer, labels = Y))
