23a45bd6219f4a6cd4447b85bb5dc11f93ae87a9
Utsal Shrestha <utsal.shres@gmail.com>
2017-12-06 01:13:03 -0800

    working version

--- research2.py
+++ research2.py
@@ -2,15 +2,14 @@ from sklearn.neural_network import MLPRegressor
 from logistic_map_inputs import get_input
 
 # Parameters
-n_sequences = 500
-
-# Get the input sequence and associated 'r'
-features, labels = get_input(n_sequences, n_input)
+# n_sequences = 500000
 
 # Network parameters
 n_input = 4
 n_layers = (4,)
 
+# Get the input sequence and associated 'r'
+features, labels = get_input(n_input)
 
 # Model
 reg = MLPRegressor(hidden_layer_sizes = n_layers, activation = 'relu', solver = 'adam',
