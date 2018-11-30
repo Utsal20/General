e644ddcdfe17b07994dffe6c74d50f746c90708c
Utsal Shrestha <utsal.shres@gmail.com>
2018-11-30 14:56:07 -0800

    implement brief preprocessing

--- closest.py
+++ closest.py
@@ -1,1 +1,15 @@
 # Divide and conquer solution
+
+def distance(a, b):
+    return ((a[0]-b[0])**2 + (a[1]-b[1])**2)**(1/2)
+
+def closest(x):
+    x = sorted(x)
+    
+
+
+
+
+
+test1 = [(2, 3), (12, 30), (40, 50), (5, 1), (12, 10), (3, 4)]
+closest(test1)
