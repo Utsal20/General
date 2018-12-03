2ed4e966448ba7d725241cb54a5b612d3fa279a6
enerelt21 <32151102+enerelt21@users.noreply.github.com>
2018-12-03 14:54:29 -0800

    Divide-n-conquer
    implemented the algorithm

--- closest.py
+++ closest.py
@@ -3,13 +3,33 @@
 def distance(a, b):
     return ((a[0]-b[0])**2 + (a[1]-b[1])**2)**(1/2)
 
+def min(a, b):
+    if (a > b):
+        a = b
+    return a
+
 def closest(x):
     x = sorted(x)
+    n = len(x)
+    if (n == 2):
+        return distance(x[0], x[1])
+
+    x1 = closest(x[:int(n/2)])
+    x2 = closest(x[(int(n/2)+1):])
+    d = min(x1, x2)
+    strip = []
+    for i in range(n-1):
+        if d > abs(x[i][0] - x[int(n/2)][0]):
+            strip.append(x[i])
     
+    strip = sorted(strip, key = lambda y: y[1])
 
-
-
-
+    k = len(strip)
+    for i in range(k-2):
+        if (d > distance(strip[i], strip[i+1])):
+            d = distance(strip[i], strip[i+1])
+    
+    return d
 
 test1 = [(2, 3), (12, 30), (40, 50), (5, 1), (12, 10), (3, 4)]
-closest(test1)
+print(closest(test1))
